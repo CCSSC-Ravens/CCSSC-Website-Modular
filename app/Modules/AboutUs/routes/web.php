594801bfc3ec647709b2321fb0139c14c0a33200
robbyt-exe <uldamamarlonrobbie.stem11a@gmail.com>
2026-01-17 13:57:37 +0800

    Revert "About Us Section and Footer"

--- app/Modules/AboutUs/routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use App\Modules\AboutUs\Http\Controllers\AboutUsController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/about-us', [AboutUsController::class, 'index'])->name('aboutus.index');
